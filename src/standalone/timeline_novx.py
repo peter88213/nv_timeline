0678e9d9d6dec8ad1028df5c856ff04d85dd9fb5
peter88213 <petert88213@gmail.com>
2025-09-04 12:58:34 +0200

    Updated docstring: Python 3.7+ required.

--- src/standalone/timeline_novx.py
+++ src/standalone/timeline_novx.py
@@ -2,7 +2,7 @@
 """Synchronize novelibre project with Timeline
 
 Version @release
-Requires Python 3.6+
+Requires Python 3.7+
 Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_timeline
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
