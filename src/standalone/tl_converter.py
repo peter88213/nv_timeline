c71362489e824f1abfe867adea922b73e4af2aed
peter88213 <petert88213@gmail.com>
2026-01-12 13:56:11 +0100

    Updated the copyright notice.

--- src/standalone/tl_converter.py
+++ src/standalone/tl_converter.py
@@ -1,6 +1,6 @@
 """Provide a converter class for test_nv_timeline.
 
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/nv_timeline
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
--- tools/make_ini.py
+++ tools/make_ini.py
@@ -3,7 +3,7 @@
 
 Create config file.
 
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/yw2xtg
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
