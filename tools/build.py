cf4d223cb654444daad0c0ab64757ea647d6a436
peter88213 <petert88213@gmail.com>
2026-03-31 21:08:40 +0200

    Version 5.9.0
    Update for novelibre 5.55 and DTD 1.10.
    
    API: 5.55
    Based on novelibre 5.55.1

--- tools/build.py
+++ tools/build.py
@@ -12,7 +12,7 @@ import sys
 sys.path.insert(0, f'{os.getcwd()}/../../novelibre/tools')
 from package_builder import PackageBuilder
 
-VERSION = '5.8.1'
+VERSION = '5.9.0'
 
 
 class PluginBuilder(PackageBuilder):
